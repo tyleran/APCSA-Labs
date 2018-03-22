94647a7ed28ede4c75556c22d68f23172f1dedf8
Tyler An <tylera63@gmail.com>
2018-03-22 11:00:30 -0700

    Elevens Activity 9

--- Unit 12/src/Lab18e.java
+++ Unit 12/src/Lab18e.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -8,24 +8,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
-import static java.lang.System.*;
 
-public class Lab18e
-{
-	public static void main( String args[] ) throws IOException
-	{
-
-
-
-
-
-
-
-
-
-
-
+public class Lab18e {
+	public static void main(String args[]) throws IOException {
+		ArrayList<Word1> words = new ArrayList<>();
+		Scanner file = new Scanner(new File("C:/Users/ant3384/Desktop/APCSA - Workspace/APCSA-Labs/Unit 12/src/lab18e.dat"));
+		int length = file.nextInt();
+		file.nextLine();
+		for (int i = 0; i < length; i++) {
+			words.add(new Word1(file.nextLine()));
+		}
+		for (int n = 0; n < words.size(); n++) {
+			for (int i = 0; i < words.size() - 1; i++)
+				if (words.get(i).compareTo(words.get(i + 1)) == -1) {
+					Word1 temp = words.get(i);
+					words.set(i, words.get(i + 1));
+					words.set(i + 1, temp);
+				}
+		}
+		for (int i = 0; i < words.size(); i++) {
+			System.out.println(words.get(i));
+		}
 
 	}
 }
+
--- Unit 12/src/Monster.java
+++ Unit 12/src/Monster.java
@@ -1,67 +1,95 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
-
-public class Monster implements Comparable
-{
+public class Monster implements Comparable {
 	private int myWeight;
 	private int myHeight;
 	private int myAge;
 
-	//write a default Constructor
-
-
-
-	//write an initialization constructor with an int parameter ht
-
-
-
-	//write an initialization constructor with int parameters ht and wt
-
-
-
-
-	//write an initialization constructor with int parameters ht, wt, and age
-
-
-
-	//modifiers - write set methods for height, weight, and age
-
-	
-	
-	//accessors - write get methods for height, weight, and age
-	
-	
-	
-	//creates a new copy of this Object
-	public Object clone()
-	{
-	   return new Monster();
+	// write a default Constructor
+	public Monster() {
+		setHeight(myHeight);
+		setWeight(myWeight);
+		setAge(myAge);
 	}
 
-	public boolean equals( Object obj )
-	{
+	// write an initialization constructor with an int parameter ht
+	public Monster(int ht) {
+		myHeight = ht;
+		myWeight = 0;
+		myAge = 0;
+	}
 
+	// write an initialization constructor with int parameters ht and wt
+	public Monster(int ht, int wt) {
+		myHeight = ht;
+		myWeight = wt;
+		myAge = 0;
+	}
 
+	// write an initialization constructor with int parameters ht, wt, and age
+	public Monster(int ht, int wt, int age) {
+		myHeight = ht;
+		myWeight = wt;
+		myAge = age;
+	}
+
+	// modifiers - write set methods for height, weight, and age
+	public void setHeight(int ht) {
+		myHeight = ht;
+	}
+
+	public void setWeight(int wt) {
+		myWeight = wt;
+	}
+
+	public void setAge(int age) {
+		myAge = age;
+	}
+
+	// accessors - write get methods for height, weight, and age
+	public int getHeight() {
+		return myHeight;
+	}
+
+	public int getWeight() {
+		return myWeight;
+	}
+
+	public int getAge() {
+		return myAge;
+	}
+
+	// creates a new copy of this Object
+	public Object clone() {
+		Monster clone = this;
+		return clone;
+	}
+
+	public boolean equals(Object obj) {
+		Monster second = (Monster) obj;
+		if (this.getHeight() == second.getHeight() && this.getWeight() == second.getWeight()
+				&& this.getAge() == second.getAge())
+			return true;
 		return false;
 	}
 
-	public int compareTo( Object obj )
-	{
-		Monster rhs = (Monster)obj;
-
-
-
-
-
-		return -1;
+	public int compareTo(Object obj) {
+		Monster rhs = (Monster) obj;
+		if (this.getHeight() > rhs.getHeight() || this.getWeight() > rhs.getWeight() || this.getAge() > rhs.getAge())
+			return 1;
+		if (this.getHeight() < rhs.getHeight() || this.getWeight() < rhs.getWeight() || this.getAge() < rhs.getAge())
+			return -1;
+		return 0;
 	}
 
-	//write a toString() method
-	
-	
+	// write a toString() method
+	public String toString() {
+		return myHeight + " " + myWeight + " " + myAge;
+	}
+
 }
+
